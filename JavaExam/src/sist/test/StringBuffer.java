d0e395a9194719bd58f397672d1f73d690060338
pukim <pukim@hanmail.net>
2015-09-18 10:11:19 -0700

    run time utils

--- JavaExam/src/sist/test/StringBuffer.java
+++ JavaExam/src/sist/test/StringBuffer.java
@@ -3,22 +3,32 @@
  */
 package sist.test;
 
+import kr.co.sist.utils.ElapsedTime;
+
 /**
  * @author owner
  *
  */
 public class StringBuffer {
 	public static void main(String[] args) {
+		ElapsedTime et = ElapsedTime.getInstance();
 		long startTime = System.nanoTime();
-	
+		
+		et.start();
 		
 		for(long l=0; l<100000000; l++) {
 			//String s = "test" + l;
-			
 			StringBuilder sb = new StringBuilder();
 			sb.append("test").append(l);
 		}
-		long endTime = System.nanoTime();
-		System.out.println("run time : " + ((endTime - startTime)/1000000.0));
+		
+		et.stop();
+		
+		long endTime = System.nanoTime(); 
+		System.out.println("run time : " + ((endTime - startTime)/1000000.0)); 
+		System.out.println("run time : " + et);
+		
+		et.unit = "S";
+		System.out.println("run time : " + et);
 	}
 }
